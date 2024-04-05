112e5ddaa47baea7998b2aac824eefdb38c434d2
maridilo <146822054+maridilo@users.noreply.github.com>
2024-04-05 12:56:45 +0200

    commit

--- SistemadeGestiondePublicaciones/src/main/java/ComparadoryContadordeContenido/Analisisdetexto.java
+++ SistemadeGestiondePublicaciones/src/main/java/ComparadoryContadordeContenido/Analisisdetexto.java
@@ -1,4 +1,38 @@
 package ComparadoryContadordeContenido;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Arrays;
+import java.util.List;
+
 public class Analisisdetexto {
+    public boolean compararArchivos(File file1, File file2) throws IOException {
+        String contenido1 = new String(Files.readAllBytes(file1.toPath()));
+        String contenido2 = new String(Files.readAllBytes(file2.toPath()));
+        return contenido1.equals(contenido2);
+    }
+    public Map<String, Integer> wordCount(File file) throws IOException {
+        String contenido = new String(Files.readAllBytes(file.toPath()));
+        String[] palabras = contenido.split("\\s+");
+        Map<String, Integer> estadisticas = new HashMap<>();
+        for (String palabra : palabras) {
+            estadisticas.put(palabra, estadisticas.getOrDefault(palabra, 0) + 1);
+        }
+        return estadisticas;
+    }
+public Map<String, Integer> wordCount(File file, String[] palabrasExcluidas) throws IOException {
+        String contenido = new String(Files.readAllBytes(file.toPath()));
+        String[] palabras = contenido.split("\\s+");
+        List<String> palabrasExcluidasList = Arrays.asList(palabrasExcluidas);
+        Map<String, Integer> estadisticas = new HashMap<>();
+        for (String palabra : palabras) {
+            if (!palabrasExcluidasList.contains(palabra)) {
+                estadisticas.put(palabra, estadisticas.getOrDefault(palabra, 0) + 1);
+            }
+        }
+        return estadisticas;
+    }
 }
--- SistemadeGestiondePublicaciones/src/main/java/Main.java
+++ SistemadeGestiondePublicaciones/src/main/java/Main.java
@@ -1,6 +1,10 @@
+import ComparadoryContadordeContenido.Analisisdetexto;
 import EditordeTextoInteractivo.Editosdetexto;
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,8 +16,36 @@ public class Main {
                 frame.setLayout(new BorderLayout());
                 frame.add(new Editosdetexto(), BorderLayout.CENTER);
                 frame.setVisible(true);
+                frame.setLocationRelativeTo(null);
+                frame.setResizable(true);
             }
         });
+
+        Analisisdetexto analisis = new Analisisdetexto();
+        File file1 = new File("ruta/al/archivo 1.txt");
+        File file2 = new File("ruta/al/archivo 2.txt");
+        try {
+            boolean sonIguales = analisis.compararArchivos(file1, file2);
+            System.out.println("Los archivos son iguales: " + sonIguales);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        File file = new File("ruta/al/archivo.txt");
+        try {
+            Map<String, Integer> wordCount = analisis.wordCount(file);
+            System.out.println("Conteo de palabras: " + wordCount);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String[] palabrasExcluidas = {"a", "de", "el", "la", "los", "las"};
+        try {
+            Map<String, Integer> wordCount = analisis.wordCount(file, palabrasExcluidas);
+            System.out.println("Conteo de palabras excluyendo palabras comunes: " + wordCount);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        }
+
     }
-}
+
 
